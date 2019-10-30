db9fcbaf049ffeee08a5d9f5131364770e5ffc0f
Sayeed Hasan <hasan.sayeed@bjitgroup.com>
2019-10-30 14:41:07 +0600

    SearchBar component modified

--- src/components/imdbSearch/SearchBar.jsx
+++ src/components/imdbSearch/SearchBar.jsx
@@ -4,12 +4,15 @@ export default class Navigation extends React.Component {
     render() {
         return (
             <div className="row m-3">
-                <form className="form-inline my-2 my-lg-0" onSubmit={(e) => this.handleFormSubmit(e)}>
-                    <input className="form-control mr-sm-2" name="search" type="search" placeholder="Search" aria-label="Search" />
-                    <button className="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
-                </form>
+                {/* <form className="form-inline my-2 my-lg-0" onSubmit={(e) => this.handleFormSubmit(e)}> */}
+                    <input onChange={()=>this.handleInputChanges()} className="form-control mr-sm-2" name="search" type="search" placeholder="Search" aria-label="Search" />
+                    {/* <button className="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button> */}
+                {/* </form> */}
             </div>
         );
+    }
+    handleInputChanges(){
+            this.props.onSubmit(document.getElementsByName("search")[0].value);
     }
     handleFormSubmit(e) {
         e.preventDefault();
